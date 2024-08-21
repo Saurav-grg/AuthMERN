9e1011cd6c00c01d60828453fc6815c90d705acc
Saurav-grg <menemy2323@gmail.com>
2024-08-21 17:12:04 +0545

    routes init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 function App() {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
