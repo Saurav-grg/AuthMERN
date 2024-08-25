4725a2bb1560954ee8f1524f92de1788b9d9a3a9
Saurav-grg <menemy2323@gmail.com>
2024-08-25 10:33:42 +0545

    login and logout routes

--- server/controller/authControl.js
+++ server/controller/authControl.js
@@ -50,27 +50,78 @@ const signup = async (req, res) => {
 const verifyEmail = async (req, res) => {
   const { code } = req.body;
   try {
-    const user = User.findOne({
+    const user = await User.findOne({
       verificationToken: code,
       verificationTokenExpiresAt: { $gt: Date.now() },
     });
     if (!user) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: 'Invalid or expired verification code',
-        });
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid or expired verification code',
+      });
     }
+
     user.isVerified = true;
     user.verificationToken = undefined;
     user.verificationTokenExpiresAt = undefined;
     await user.save();
 
     await sendWelcomeEmail(user.email, user.name);
-  } catch (error) {}
+    res.status(200).json({
+      success: true,
+      message: 'Email verified successfully',
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log('error in verifyEmail ', error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
+const logout = async (req, res) => {
+  res.clearCookie('token');
+  res.status(200).json({ success: true, message: 'Logged out successfully' });
+};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid credentials' });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid credentials' });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: 'Logged in successfully',
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log('Error in login ', error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 module.exports = {
   signup,
   verifyEmail,
+  logout,
+  login,
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { signup, verifyEmail } = require('../controller/authControl');
+const {
+  signup,
+  verifyEmail,
+  logout,
+  login,
+} = require('../controller/authControl');
 
 router.post('/sign-up', signup);
 router.post('/verify-email', verifyEmail);
+router.post('/logout', logout);
+router.post('/login', login);
 
 module.exports = router;
